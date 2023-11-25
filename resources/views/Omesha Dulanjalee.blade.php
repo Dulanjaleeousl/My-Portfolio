990f6c6fea202a1fc334fdb7bffb1f26cdb7be25
Dulanjaleeousl <90521067+Dulanjaleeousl@users.noreply.github.com>
2023-11-25 23:08:13 +0530

    Update Omesha Dulanjalee.blade.php

--- resources/views/Omesha Dulanjalee.blade.php
+++ resources/views/Omesha Dulanjalee.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Omesha Dulanjalee</title>
-  <link href="img/O-logo.png" rel="icon">
+  <link href="img/Om-logo.png" rel="icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
@@ -1192,7 +1192,7 @@ h4 svg:hover {
   <!-- ======= Summary Section ======= -->
   <section id="summary" class="d-flex flex-column">
     <div class="summary-container">
-      <h1><img src="{{ asset('img/O-logo.png') }}">mesha Dulanjalee</h1>
+      <h1><img src="{{ asset('img/Om-logo.png') }}">mesha Dulanjalee</h1>
       <p id="typedText">I'm <span class="typed"></span></p>
     </div>
     <a class="cv-download" href="Omesha Dulanjalee Resume.pdf" download="Omesha Dulanjalee Resume"><svg
